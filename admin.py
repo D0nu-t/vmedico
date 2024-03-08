1b01cc84ed5c2b55999469ecc9066b1a17a28ad9
Krishnasai Addala <75469758+D0nu-t@users.noreply.github.com>
2024-03-08 18:45:57 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Doctor, Patient, Appointment, Prescription, Profile, HealthRecord, LabResult
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+# admin.site.register(Appointment)
+admin.site.register(Prescription)
+admin.site.register(Profile)
+admin.site.register(HealthRecord)
+admin.site.register(LabResult)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'date_time', 'notes')
+    list_filter = ('doctor', 'patient', 'date_time')
+    search_fields = ('doctor__username', 'patient__username', 'notes')
+
+admin.site.register(Appointment, AppointmentAdmin)
